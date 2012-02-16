c0c7e7f3c1f90cc8c7256ee3295ecb6592ddf240
Mattias Hising <hising@gmail.com>
2012-02-16 10:51:20 +0100

    working hello world example

---
+++ app/App.php
@@ -0,0 +1,80 @@
+<?
+    /* Paths start */
+
+	define('ROOT', dirname(__FILE__) . '/../');
+	define('EXTERNALS_ROOT', ROOT . '/externals/');
+	define('TEMPLATE_ROOT', ROOT . '/templates/');
+
+    set_include_path(get_include_path() . PATH_SEPARATOR . implode(PATH_SEPARATOR, array(ROOT, EXTERNALS_ROOT)));
+    /* Paths end */
+
+    require "slim/Slim.php";
+    require "mustache/Mustache.php";
+    
+    class App {
+        
+        private $mustache, $templates, $template_path;
+        public $slim; //If you wanna go directly at $app->slim->somehting
+        
+        public function __construct(){
+            $this->slim = new Slim();
+            $this->template_path = TEMPLATE_ROOT;
+            $this->templates = array(); //Fix later
+        }
+        
+        public function render($view, $data = array(), $layout = 'layout'){
+            
+            if($this->slim->request()->headers('X-Requested-With')) {
+                $content = json_encode($data);
+                return $content;
+            }
+            
+            $this->mustache = new Mustache();
+            
+            $view = str_replace('_', '/', $view);
+            $layout = str_replace('_', '/', $layout);
+
+            $content = "";      
+                  
+            $layout_template = $this->_read_template($layout);
+            $view_template = $this->_read_template($view);
+            
+            if(array_key_exists('partials', $this->templates)) {
+                $data['content'] = $this->mustache->render($view_template, $data, $this->templates['partials']);
+                $content = $this->mustache->render($layout_template, $data, $this->templates['partials']);
+            } else {
+                $data['content'] = $this->mustache->render($view_template, $data);
+                $content = $this->mustache->render($layout_template, $data);
+            }
+            
+            return $content;
+        }
+        
+        public function get($route, $callback){
+            
+            /*
+                Check if Ajax-request, if so respond with JSON
+            */
+            
+            return $this->slim->get($route, $callback);
+        }
+        
+        public function post($route, $callback){
+            return $this->slim->post($route, $callback);
+        }
+        
+        public function run(){
+            $this->slim->run();
+        }
+        
+        private function _read_template($template){
+            $file_content = "";
+           
+            $file_name = $this->template_path . '/' . $template . '.html';
+            if(file_exists($file_name)){
+                $file_content = file_get_contents($file_name);
+            }
+            return $file_content;
+        }
+    }
+?>
---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?php
+    require '../app/App.php';
+    
+    $app = new App();
+    
+    $app->get('/hello/(:name)', function($name = 'world') use ($app){
+        echo $app->render('world', array('name' => $name));
+    });
+    
+    $app->run();
+?>
