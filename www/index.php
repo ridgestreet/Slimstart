b8cd4d458cab2ac9e274d46f66ba1947d3df7195
Mattias Hising <hising@gmail.com>
2012-02-16 15:38:33 +0100

    fixed css and startpage

--- www/index.php
+++ www/index.php
@@ -4,6 +4,10 @@
     
     $app = new App();
     
+    $app->get('/', function() use ($app){
+        echo $app->render('index');
+    });
+    
     $app->get('/hello/(:name)', function($name = 'world') use ($app){
         echo $app->render('world', array('name' => $name));
     });
