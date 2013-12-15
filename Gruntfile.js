2cad540517194f04e5d2352ff7ee32afc6871151
Mattias Hising <hising@gmail.com>
2013-12-15 14:40:04 +0100

    now with uglify support for minification of js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
   // Your grunt code goes in here.
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     lint: {
       files: [
         'www/js/vendors/jquery.1.7.1.js',
@@ -11,19 +12,29 @@ module.exports = function(grunt) {
     concat: {
       dist: {
         src: '<%= lint.files %>',
-        dest: 'www/js/dist/slimstart.js',
+        dest: 'www/js/dist/<%= pkg.name %>.js',
         separator: ';'
       }
     },
     min: {
       dist: {
-        src: ['www/js/dist/slimstart.js'],
-        dest: 'www/js/dist/slimstart.min.js'
+        src: ['www/js/dist/<%= pkg.name %>.js'],
+        dest: 'www/js/dist/<%= pkg.name %>.min.js'
       }
     },
     watch: {
       files: ['<%= lint.files %>'],
-      tasks: ['concat']
+      tasks: ['concat', 'uglify']
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-dd-mm") %> */\n'
+      },
+      dist: {
+        files: {
+          'www/js/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
     }
   });
   
